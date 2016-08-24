a1c5b040bab0bcc31d1ec3d54278fbed9d2dd20b
Alessandro Febretti <febret@gmail.com>
2016-08-23 22:27:00 -0500

    pack update

--- pack.cmake
+++ pack.cmake
@@ -1,19 +1,24 @@
 pack_native_module(omium)
-if(WIN32)
-    pack_shared_lib(d3dcompiler_43)
-    pack_shared_lib(d3dcompiler_47)
+
+if(APPLE)
+    pack_binary_file("Chromium Embedded Framework")
+    pack_binary_dir("Resources")
+else()
+    if(WIN32)
+        pack_library(d3dcompiler_43)
+        pack_library(d3dcompiler_47)
+    endif()
+
+    pack_library(libcef)
+    pack_library(libEGL)
+    pack_library(libGLESv2)
+
+    pack_binary_file(cef.pak)
+    pack_binary_file(cef_100_percent.pak)
+    pack_binary_file(cef_200_percent.pak)
+    pack_binary_file(cef_extensions.pak)
+    pack_binary_file(devtools_resources.pak)
+    pack_binary_file(icudtl.dat)
+    pack_binary_dir(locales)
 endif()
-
-pack_shared_lib(libcef)
-pack_shared_lib(libEGL)
-pack_shared_lib(libGLESv2)
-
-pack_binary_file(cef.pak)
-pack_binary_file(cef_100_percent.pak)
-pack_binary_file(cef_200_percent.pak)
-pack_binary_file(cef_extensions.pak)
-pack_binary_file(devtools_resources.pak)
-pack_binary_file(icudtl.dat)
-pack_binary_dir(locales)
-
 
